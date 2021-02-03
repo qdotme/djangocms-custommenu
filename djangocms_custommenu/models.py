52c76013ede6b40e12c0dd70e842c2bd5cf69689
Tom Mloduchowski <q@qdot.me>
2021-02-03 18:59:49 -0500

    Switch to six to support Django 3.0+ which deprecates own implementation

--- djangocms_custommenu/models.py
+++ djangocms_custommenu/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.conf import settings
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
 
